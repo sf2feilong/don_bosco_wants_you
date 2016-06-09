cfe86939c702644cfa48e967d1e47387d22ba2f8
sf2feilong <sf2feilong@gmail.com>
2016-06-09 14:51:04 +0800

    added index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mega Man X Vault</title>
+</head>
+</html>
+<?php
+    include 'logo_header.php';
+	include 'nav_header.php';
+	include 'index_content.php';
+	include 'footer.php';
+?>	
